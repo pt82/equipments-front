8e7be2462454db3dbca2a9ae50b853bef2a8db6a
Marat Bulatov <bulmarik@yandex.ru>
2023-07-05 22:56:24 +0700

    у

--- src/components/Report/ReportChar.vue
+++ src/components/Report/ReportChar.vue
@@ -9,7 +9,7 @@
           <p class="guildmemberCharName">{{ item.name }}</p>
         </div> -->
 
-        // Разбираемся...
+        // Разобрались, нет?
         <div class="guildmemberChars" v-for="char in item.chars" :key="char.id">
           <p class="guildmemberCharRelict">{{ char.relict }}р</p>
           <p class="guildmemberCharName">| {{ char.name }}</p>
